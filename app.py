f743a7d5b90dd6bd358bf79ab503fae8bf95a2d4
blankhall98 <blankhall@ciencias.unam.mx>
2024-03-01 00:01:16 +0100

    Sesion 2

--- app.py
+++ app.py
@@ -57,6 +57,27 @@ def definir_tasas():
 
     return render_template('tasas.html')
 
+@app.route('/calcular_cambio',methods=['GET', 'POST'])
+def calcular_cambio():
+    if request.method == 'POST':
+        tipo_cambio = request.form.get('tipo_cambio')
+        monto = float(request.form.get('monto'))
+
+        # Obtener el último registro de TasaDeCambio
+        ultima_tasa = TasaDeCambio.query.order_by(TasaDeCambio.id.desc()).first()
+
+        if tipo_cambio == 'venta':
+            tasa = ultima_tasa.tasa_venta
+        elif tipo_cambio == 'compra':  # compra
+            tasa = ultima_tasa.tasa_compra
+
+        # Calcular el cambio
+        cambio = monto * tasa
+
+        return render_template('resultado_cambio.html', cambio=cambio, monto=monto, tipo_cambio=tipo_cambio)
+
+    return render_template('calcular_cambio.html')
+
 #correrr la aplicación -------------------------------------------
 if __name__ == '__main__':
     with app.app_context():
