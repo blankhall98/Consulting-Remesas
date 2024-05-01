c2ee256a979470427be1b03ee6f6ed7d8c50450c
Rigoberto <rigoberto7711@gmail.com>
2024-05-01 10:28:29 -0600

    actualizacion1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\52998\Desktop\plataforma\venv
+set VIRTUAL_ENV=C:\Users\Rigoberto\Desktop\Arqueos_Desarrollo\Consulting-Remesas-030424\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
